dd6c0448b6fb0aec5051d43d4b6d1e240c7a7419
Caligari <caligari@mg4ever.net>
2019-06-21 09:55:50 +0200

    Ajout d'un dico francais / tests sur la distance de Levenshtein

--- algo kNN/words.py
+++ algo kNN/words.py
@@ -1,4 +1,7 @@
-def levenshtein(chaine1, chaine2):
+import time
+
+def levenshtein(chaine1, chaine2, effacement = 1, insertion = 1, substitution = 1, permutation = 1):
+    """Renvoit la distance de Levenshtein entre les deux chaines passées en paramètres"""
     l1 = len(chaine1)
     l2 = len(chaine2)
 
@@ -15,35 +18,46 @@ def levenshtein(chaine1, chaine2):
             if chaine1[i-1] == chaine2[j-1]:
                 cout = 0
             else:
-                cout = 1
-            d[i][j] = min( d[i-1][j] + 1,       # effacement du nouveau caractère de chaine1
-                           d[i][j-1] + 1,       # insertion dans chaine2 du nouveau caractère de chaine1
-                           d[i-1][j-1] + cout   # substitution
+                cout = substitution
+            d[i][j] = min( d[i-1][j] + effacement,      # effacement du nouveau caractère de chaine1
+                           d[i][j-1] + insertion,       # insertion dans chaine2 du nouveau caractère de chaine1
+                           d[i-1][j-1] + cout           # substitution
                          )
-    print(d)
+            # Permutation
+            if i > 1 and j > 1 and chaine1[i-1] == chaine2[j-2] and chaine1[i-2] == chaine2[j-1]:
+                d[i][j] = min( d[i][j], d[i-2][j-2] + permutation )
+
+    return d[l1][l2]
+
+f = open("french.txt")
+liste = f.read().split("\n")
+f.close()
+
+# liste=liste[:5000]
+
+print("On a %d mots en stock" % len(liste))
+print("Allant de %s à %s\n" % (liste[0], liste[-1]) )
+
+test = "odrinateir"
+
+r = []
+
+start = time.time()
+
+for m in liste:
+    dist = levenshtein(m, test)
+    r.append( (dist, m) )
+
+t_liste = time.time()
+
+r.sort(key = lambda x: x[0])
+
+t_sort = time.time()
+
+print("Vous avez tapé '%s'" % test)
+
+print("    voici les 10 mots les plus proches:")
+for p in r[:10]:
+    print("       dist. = %d : '%s'" % p )
     
-    return	d[l1][l2]
-    """d est un tableau de longueurChaine1+1 rangées et longueurChaine2+1 colonnes
-      d est indexé à partir de 0, les chaînes à partir de 1
-      déclarer entier d[0..longueurChaine1, 0..longueurChaine2]
-	  i et j itèrent sur chaine1 et chaine2
-    déclarer entier i, j, coûtSubstitution
- 
-   pour i de 0 à longueurChaine1
-       d[i, 0] := i
-   pour j de 0 à longueurChaine2
-       d[0, j] := j
- 
-   pour i de 1 à longueurChaine1
-      pour j de 1 à longueurChaine2
-          si chaine1[i-1] = chaine2[j-1] alors coûtSubstitution := 0
-                                     sinon coûtSubstitution := 1    
-           d[i, j] := minimum(
-                                d[i-1, j  ] + 1,                 // effacement du nouveau caractère de chaine1
-                                d[i,   j-1] + 1,                 // insertion dans chaine2 du nouveau caractère de chaine1
-                                d[i-1, j-1] + coûtSubstitution   // substitution
-                             )
- 
-   renvoyer d[longueurChaine1, longueurChaine2]"""
-   
-print( levenshtein("niche", "chiens") )
+print("\n Calcul des distances : %.3fs (~%d mots/sec)\n Classement des résultats : %.3fs" % (t_liste-start, len(liste)/(t_liste-start), t_sort-t_liste) )
